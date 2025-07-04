ab80257464dbb8d4c628fa3cfa702658b4f85f9a
kaucrow <kaucrow@protonmail.com>
2025-07-03 23:15:45 -0400

    Add project files.

---
+++ src/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Acadnetmy.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using Acadnetmy.MVVM;
+
+namespace Acadnetmy.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            
+        }
+
+        private Item selectedItem;
+
+        public Item SelectedItem
+        {
+            get { return selectedItem; }
+            set {
+                selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
